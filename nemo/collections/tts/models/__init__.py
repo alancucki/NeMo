bb66ad0cb949eab426b3fd510525e4edcf0fdace
Stanislav Beliaev <stasbelyaev96@gmail.com>
2021-02-11 23:44:35 +0300

    TalkNet 1.x draft.
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>

--- nemo/collections/tts/models/__init__.py
+++ nemo/collections/tts/models/__init__.py
@@ -18,7 +18,7 @@ from nemo.collections.tts.models.glow_tts import GlowTTSModel
 from nemo.collections.tts.models.melgan import MelGanModel
 from nemo.collections.tts.models.squeezewave import SqueezeWaveModel
 from nemo.collections.tts.models.tacotron2 import Tacotron2Model
-from nemo.collections.tts.models.talknet import TalkNetDursModel, TalkNetSpectModel
+from nemo.collections.tts.models.talknet import TalkNetDursModel, TalkNetPitchModel, TalkNetSpectModel
 from nemo.collections.tts.models.two_stages import GriffinLimModel, MelPsuedoInverseModel, TwoStagesModel
 from nemo.collections.tts.models.uniglow import UniGlowModel
 from nemo.collections.tts.models.waveglow import WaveGlowModel
@@ -34,6 +34,7 @@ __all__ = [
     "GriffinLimModel",
     "MelPsuedoInverseModel",
     "TalkNetDursModel",
+    "TalkNetPitchModel",
     "TalkNetSpectModel",
     "UniGlowModel",
     "MelGanModel",
